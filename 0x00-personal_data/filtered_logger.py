418f69d7bb5913f8a3b413b44391da4beab28e29
JeremyWarui <jerywarne7@gmail.com>
2023-08-03 14:17:16 +0000

    Filter data function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,6 +9,6 @@ def filter_datum(fields, redaction, message, separator):
     """function that returns the log message obfudcated"""
     for arg in fields:
         message = re.sub(r'{}=.+?{}'.format(arg, separator),
-                '{}={}{}'.format(arg, redaction, separator),
-                message)
+                         '{}={}{}'.format(arg, redaction, separator),
+                         message)
     return message
