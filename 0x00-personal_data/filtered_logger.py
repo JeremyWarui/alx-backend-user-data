5c87c34716d0feac94d411bc71cb8f039abf2b9a
JeremyWarui <jerywarne7@gmail.com>
2023-08-03 14:10:07 +0000

    Filter data function

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+filtered logger file for personal data
+"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """function that returns the log message obfudcated"""
+    for arg in fields:
+        message = re.sub(r'{}=.+?{}'.format(arg, separator),
+                '{}={}{}'.format(arg, redaction, separator),
+                message)
+    return message
