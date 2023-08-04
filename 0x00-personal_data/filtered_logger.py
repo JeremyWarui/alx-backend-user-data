1190f43ec1ced42a962a45d443b5f36c1fc755dc
JeremyWarui <jerywarne7@gmail.com>
2023-08-04 06:56:41 +0000

    Create logger object

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,13 +2,13 @@
 """
 filtered logger file for personal data
 1. class RedactingFormatter
-2. filter_datum function
+2. filter_datum function returning log message
+3. get_logger function returninh logging.Logger
 """
 import re
 from typing import List
 import logging
-PII_FIELDS = ("name", "email", "phone", "ssn", "password", "ip",
-              "last_login", "user_agent")
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 class RedactingFormatter(logging.Formatter):
@@ -37,3 +37,14 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                          "{}={}{}".format(arg, redaction, separator),
                          message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """returns a logging.Logger object"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
