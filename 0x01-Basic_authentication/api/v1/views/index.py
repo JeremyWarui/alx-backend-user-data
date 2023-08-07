82a88e6277a7f1a2c34e0960f3b652a5efd64d57
JeremyWarui <jerywarne7@gmail.com>
2023-08-07 23:14:01 +0300

    Adding forbidden error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized_route() -> str:
     """GET /api/v1/unauthorized"""
     abort(401)
+
+
+@app_views.route("/forbidden", strict_slashes=False)
+def forbidden_route() -> str:
+    """GET /api/v1/forbidden route"""
+    abort(403)
