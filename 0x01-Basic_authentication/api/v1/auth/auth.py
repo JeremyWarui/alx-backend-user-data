c6c4f58f82c5710e0b9a089292e1ae7af6f22bec
JeremyWarui <jerywarne7@gmail.com>
2023-08-07 23:52:58 +0300

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,11 @@ class Auth():
     def authorization_header(self, request=None) -> str:
         """Checks the authorisation header for authorisation
         Returns None, or request header"""
-        return None
+        if request is None:
+            return None
+        if not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar("User"):
         """checks current user/session
