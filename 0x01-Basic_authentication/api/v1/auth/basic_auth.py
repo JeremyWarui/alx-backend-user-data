d2f5312e07c560d5fb71b485ec8b427488bbaf3c
JeremyWarui <jerywarne7@gmail.com>
2023-08-09 22:31:59 +0300

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,15 @@ class BasicAuth(Auth):
             return decoded_val
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """returns user email and password from Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        personal_data = decoded_base64_authorization_header.split(":", 1)
+        return personal_data[0], personal_data[1]
