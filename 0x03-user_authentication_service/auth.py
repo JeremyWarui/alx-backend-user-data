722192a7641c55a86fc69b93e28175ec944a2591
JeremyWarui <jerywarne7@gmail.com>
2023-08-18 00:12:41 +0300

    Session functions and the basic application to implement session authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -54,7 +54,7 @@ class Auth:
         assign to user's session id property
         and returns the sessionID"""
         try:
-            found_user = self._db.find_user_by(email)
+            found_user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
             self._db.update_user(found_user.id, session_id=session_id)
             return session_id
